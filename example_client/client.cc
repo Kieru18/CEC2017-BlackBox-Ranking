24650df7ed0bee0d9646214332d50d869171071c
jkierucz <01169183@pw.edu.pl>
2024-04-17 23:21:23 +0200

    added simple client
    compile with:
    g++ -o client client.cc -lboost_system -lboost_thread -pthread

--- example_client/client.cc
+++ example_client/client.cc
@@ -4,59 +4,59 @@
 
 using boost::asio::ip::tcp;
 
+std::string generate_request() {
+    std::string json_data = "{\"function\": \"sum\", \"numbers\": [1, 7, 3]}";
+    return "POST /calculate HTTP/1.1\r\n"
+           "Host: localhost:8080\r\n"
+           "Content-Type: application/json\r\n"
+           "Content-Length: " + std::to_string(json_data.length()) + "\r\n"
+           "Connection: close\r\n\r\n"
+           + json_data;
+}
+
 int main() {
     try {
         boost::asio::io_context io_context;
-
-        // Resolve endpoint
-        tcp::resolver resolver(io_context);
-        tcp::resolver::results_type endpoints = resolver.resolve("localhost", "8080");
-
-        // Establish connection
         tcp::socket socket(io_context);
-        boost::asio::connect(socket, endpoints);
+        tcp::resolver resolver(io_context);
 
-        // Prepare request JSON
-        std::string request = R"({"function": "sum", "numbers": [1, 7, 3]})";
+        boost::asio::connect(socket, resolver.resolve("localhost", "8080"));
+
+        // Prepare request
+        std::string request = generate_request();
 
         // Send request
-        boost::asio::write(socket, boost::asio::buffer("POST /calculate HTTP/1.1\r\n"));
-        boost::asio::write(socket, boost::asio::buffer("Host: localhost\r\n"));
-        boost::asio::write(socket, boost::asio::buffer("Content-Type: application/json\r\n"));
-        boost::asio::write(socket, boost::asio::buffer("Content-Length: " + std::to_string(request.size()) + "\r\n"));
-        boost::asio::write(socket, boost::asio::buffer("\r\n"));
         boost::asio::write(socket, boost::asio::buffer(request));
 
         // Read response
-        boost::asio::streambuf response_buf;
-        boost::asio::read_until(socket, response_buf, "\r\n");
-        std::istream response_stream(&response_buf);
+        boost::asio::streambuf response;
+        boost::asio::read_until(socket, response, "\r\n");
+
+        // Process and print response
+        std::istream response_stream(&response);
         std::string http_version;
         response_stream >> http_version;
         unsigned int status_code;
         response_stream >> status_code;
+
         std::string status_message;
         std::getline(response_stream, status_message);
 
         if (!response_stream || http_version.substr(0, 5) != "HTTP/") {
-            std::cerr << "Invalid response\n";
+            std::cout << "Invalid response\n";
             return 1;
         }
 
         if (status_code != 200) {
-            std::cerr << "Response returned with status code " << status_code << "\n";
+            std::cout << "Response returned with status code " << status_code << "\n";
             return 1;
         }
 
-        // Read and print response body
-        boost::asio::read_until(socket, response_buf, "\r\n\r\n");
-        std::string header;
-        while (std::getline(response_stream, header) && header != "\r") {}
-        std::ostringstream response_data;
-        if (response_buf.size() > 0) {
-            response_data << &response_buf;
-        }
-        std::cout << "Response from server: " << response_data.str() << "\n";
+        // Read the rest of the response
+        boost::asio::read_until(socket, response, "\r\n\r\n");
+
+        // Output the response
+        std::cout << &response << std::endl;
 
     } catch (std::exception& e) {
         std::cerr << "Exception: " << e.what() << "\n";
