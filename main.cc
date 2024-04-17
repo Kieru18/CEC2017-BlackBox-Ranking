b42f4469e3368e0fe33581b7a850ce1dd1c56e3e
jkierucz <01169183@pw.edu.pl>
2024-04-17 21:55:25 +0200

    updated version, mitigates end of file exception

--- main.cc
+++ main.cc
@@ -13,19 +13,33 @@ void handle_client(std::shared_ptr<tcp::socket> socket) {
         boost::system::error_code error;
 
         // Reading the request
-        boost::asio::read_until(*socket, request, "\r\n\r\n", error);
-        if (error) throw boost::system::system_error(error);
+        while (boost::asio::read_until(*socket, request, "\r\n\r\n", error)) {
+            if (error && error != boost::asio::error::eof) {
+                throw boost::system::system_error(error);
+            } else if (error == boost::asio::error::eof) {
+                // Client closed the connection
+                std::cerr << "Client closed the connection.\n";
+                return;  // Clean exit on client connection close
+            }
 
-        // Sending a response
-        std::string response =
-            "HTTP/1.1 200 OK\r\n"
-            "Content-Type: text/plain\r\n"
-            "Content-Length: 12\r\n"
-            "Connection: close\r\n\r\n"
-            "Hello World!";
+            // Prepare a response
+            std::string response =
+                "HTTP/1.1 200 OK\r\n"
+                "Content-Type: text/plain\r\n"
+                "Content-Length: 12\r\n"
+                "Connection: close\r\n\r\n"
+                "Hello World!";
 
-        boost::asio::write(*socket, boost::asio::buffer(response), error);
+            // Sending a response
+            boost::asio::write(*socket, boost::asio::buffer(response), error);
+            if (error) {
+                throw boost::system::system_error(error);
+            }
 
+            // Since the response has been sent and connection is to be closed,
+            // break the loop to prevent further reads.
+            break;
+        }
     } catch (std::exception& e) {
         std::cerr << "Exception in thread: " << e.what() << "\n";
     }
@@ -41,7 +55,6 @@ int main() {
 
         // Infinite loop to serve requests
         while (true) {
-            // Use shared_ptr to manage the socket's lifetime
             auto socket = std::make_shared<tcp::socket>(io_context);
             acceptor.accept(*socket);
 
